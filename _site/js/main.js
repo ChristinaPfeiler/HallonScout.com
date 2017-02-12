0d67f945f1b89f49068f7774ed1ae662d5e5339f
Christina.Papillon <christinapfeiler@hotmail.com>
2017-02-12 04:08:38 +0100

    Site update Version1
    Portfolio work day 2 - 9

---
+++ _site/js/main.js
@@ -0,0 +1,10 @@
+//Document ready
+//add this function to make sure your page has completely loaded, before any js/jquery runs
+
+$(document).ready( function () {
+
+//Slick carousel slider Startpage
+$(".slider").slick();
+
+
+}); //End of document - ready
